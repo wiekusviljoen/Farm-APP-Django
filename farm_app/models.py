23b7083e315c28c77a5193c4f7516d91c9a6a70a
wiekusviljoen <wiekusviljoen@gmail.com>
2024-02-11 22:45:57 +0200

    commit on master

--- farm_app/models.py
+++ farm_app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+
+CATEGORY =(
+    ('Bonsmara', 'Bonsmara'),
+    ('Brahman', 'Brahman'),
+    ('Jersey', 'Jersey'),
+)
+
 class Cow(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -16,9 +23,11 @@ class Bull(models.Model):
 class Farm(models.Model):
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100,choices=CATEGORY,null = True)
     cows_count = models.IntegerField(default=0)  # Number of cows
     bulls_count = models.IntegerField(default=0)  # Number of bulls
+    calf_count = models.IntegerField(default=0)
 
     @property
     def total_cattle(self):
-        return self.cows_count + self.bulls_count
+        return self.cows_count + self.bulls_count + self.calf_count
